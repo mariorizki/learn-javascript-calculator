bcce47df889c28e4384c7359d9a3b695f26143af
Mario Rizki <mariorizki07@gmail.com>
2019-01-30 08:54:30 +0700

    Create Initial JavaScript

---
+++ index.js
@@ -0,0 +1,29 @@
+// Add
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// Substract
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+// Multiply
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// Divide
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// Modulo
+
+function modulo(num1, num2) {
+  return num1 % num2;
+}
